4b8ef8ca674c20e1afbf33f0c5128683a793bf22
paul-laird <35932444+paul-laird@users.noreply.github.com>
2024-09-27 09:32:46 +0100

    Euler function problem definition

--- euler.js
+++ euler.js
@@ -2,7 +2,13 @@
 of 3 or 5, we get 3, 5, 6 and 9.
 The sum of these multiples is 23.
 
-Find the sum of all the multiples of 3 or 5 below 1000.
+1. Find the sum of all the multiples of 3 or 5 below 1000.
+
+2. Create a function to find the sum of all the multiples of a or b below n.
+(a,b,n) are function parameters
+
+3. Allow the user to enter the parameters via text box(es)
+and alert them to the output
 */
 alert("Loaded Euler JS");
 a=3;
