cd626d8c0f7b281af67882ef050ecfb7640fb388
paul-laird <35932444+paul-laird@users.noreply.github.com>
2024-09-27 08:28:46 +0100

    Introduce Euler

---
+++ euler.js
@@ -0,0 +1,11 @@
+/*If we list all the natural numbers below 10 that are multiples
+of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+alert("Loaded Euler JS");
+a=3;
+b=5;
+n=1000;
+
