18326c56c249b8fc23ccb3c1aad486b3b123f8b2
paul-laird <35932444+paul-laird@users.noreply.github.com>
2024-10-04 09:22:17 +0100

    typo

--- euler.js
+++ euler.js
@@ -24,13 +24,13 @@ b.
 Create a function which takes parameters a,l
 a is a list of two integers
 l is a list of integers
-find the sum of all the multiples of a or b in l
+find the sum of all the multiples of elements of a in l
 
 c.
 Create a function which takes parameters a,l
 a is a list of integers
 l is a list of integers
-find the sum of all the multiples of a or b in l
+find the sum of all the multiples of elements of a in l
 */
 alert("Loaded Euler JS");
 a=3;
