a56518077e64fc373294760cf00179a9214c64e2
paul-laird <35932444+paul-laird@users.noreply.github.com>
2024-10-07 11:27:24 +0100

    basket exercise

---
+++ basket.js
@@ -0,0 +1,16 @@
+alert("Loaded Basket JS");
+
+/*
+Create a function that takes two objects,
+basket and prices as parameters
+where basket is a mapping from products (string) to quantities (number)
+and prices is a mapping from products(String) to prices (number)
+and returns the total cost of the basket
+*/
+
+let basketCost=(b,p)=>{
+    alert("doing something");
+    return 0; //clearly wrong!
+}
+
+alert(basketCost({},{}))
